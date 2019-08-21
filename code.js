cb6c5e89c7bfb64b6848e4cd079697444fbba46f
matthewfamularo <matthewfamularo@icloud.com>
2019-08-21 10:55:23 -0400

    switches to using figma helper function

--- code.js
+++ code.js
@@ -1,18 +1,20 @@
-for (var _i = 0, _a = figma.currentPage.children; _i < _a.length; _i++) {
-    var node = _a[_i];
-    //First, find all layers whose names include "textGroup", and ensure they have only two children
-    if (node.name.includes("textGroup") && node.children.length == 2) {
-        //Set the paddingAmount based on the number specified in the layer name
-        var paddingAmount = parseInt(node.name.split("-")[1]);
-        //Figure out which element is on the right and which is on the left
-        if (node.children[0].x < node.children[1].x) {
-            //The first element is on the left, adjust the x value of the second element accordingly
-            node.children[1].x = (node.children[0].x + node.children[0].width + paddingAmount);
-        }
-        else {
-            //The first element is on the right, adjust the x value of the first element accordingly
-            node.children[0].x = (node.children[1].x + node.children[1].width + paddingAmount);
-        }
+//Use Figma's traversal helper function to find all textGroup groups
+var nodes = figma.currentPage.findAll(function (node) { return node.name.includes("textGroup") && node.children.length == 2 && node.type === "GROUP"; });
+console.log(nodes);
+for (var _i = 0, nodes_1 = nodes; _i < nodes_1.length; _i++) {
+    var node = nodes_1[_i];
+    //Set the paddingAmount based on the number specified in the layer name
+    var paddingAmount = parseInt(node.name.split("-")[1]);
+    //Figure out which element is on the right and which is on the left
+    if (node.children[0].x < node.children[1].x) {
+        //The first element is on the left, adjust the x value of the second element accordingly
+        node.children[1].x =
+            node.children[0].x + node.children[0].width + paddingAmount;
+    }
+    else {
+        //The first element is on the right, adjust the x value of the first element accordingly
+        node.children[0].x =
+            node.children[1].x + node.children[1].width + paddingAmount;
     }
 }
 figma.closePlugin();
--- code.ts
+++ code.ts
@@ -1,19 +1,20 @@
-for (const node of figma.currentPage.children) {
-  
-}
-for (const node of figma.currentPage.children) {
-  //First, find all layers whose names include "textGroup", and ensure they have only two children
-  if(node.name.includes("textGroup") && node.children.length == 2 ){
-    //Set the paddingAmount based on the number specified in the layer name
-    let paddingAmount = parseInt(node.name.split("-")[1]);
-    //Figure out which element is on the right and which is on the left
-    if(node.children[0].x < node.children[1].x){
-      //The first element is on the left, adjust the x value of the second element accordingly
-      node.children[1].x = (node.children[0].x + node.children[0].width + paddingAmount);
-    }else{
-      //The first element is on the right, adjust the x value of the first element accordingly
-      node.children[0].x = (node.children[1].x + node.children[1].width + paddingAmount);
-    }
+  //Use Figma's traversal helper function to find all textGroup groups
+const nodes = figma.currentPage.findAll(
+  node => node.name.includes("textGroup") && node.children.length == 2 && node.type === "GROUP"
+);
+console.log(nodes);
+for (const node of nodes) {
+  //Set the paddingAmount based on the number specified in the layer name
+  let paddingAmount = parseInt(node.name.split("-")[1]);
+  //Figure out which element is on the right and which is on the left
+  if (node.children[0].x < node.children[1].x) {
+    //The first element is on the left, adjust the x value of the second element accordingly
+    node.children[1].x =
+      node.children[0].x + node.children[0].width + paddingAmount;
+  } else {
+    //The first element is on the right, adjust the x value of the first element accordingly
+    node.children[0].x =
+      node.children[1].x + node.children[1].width + paddingAmount;
   }
 }
-figma.closePlugin()
+figma.closePlugin();
