c16fb16788f00b53451732a0f1dd86de9c80be91
matthewfamularo <matthewfamularo@icloud.com>
2019-08-21 17:16:53 -0400

    adds relation checking

--- code.js
+++ code.js
@@ -11,19 +11,47 @@ for (var _i = 0, nodes_1 = nodes; _i < nodes_1.length; _i++) {
     var paddingAmount = parseInt(node.name.split("-")[1]);
     //Figure out which element is on the right and which is on the left
     //Also ensures that the items aren't already in the right place
-    if (node.children[0].x < node.children[1].x &&
-        node.children[0].x + node.children[0].width + paddingAmount !==
-            node.children[1].x) {
-        //The first element is on the left, adjust the x value of the second element accordingly
-        node.children[1].x =
-            node.children[0].x + node.children[0].width + paddingAmount;
+    if (relation(node.children) == "horizontal") {
+        if (node.children[0].x < node.children[1].x &&
+            node.children[0].x + node.children[0].width + paddingAmount !==
+                node.children[1].x) {
+            //The first element is on the left, adjust the x value of the second element accordingly
+            node.children[1].x =
+                node.children[0].x + node.children[0].width + paddingAmount;
+        }
+        else if (node.children[0].x > node.children[1].x &&
+            node.children[1].x + node.children[1].width + paddingAmount !==
+                node.children[0].x) {
+            //The first element is on the right, adjust the x value of the first element accordingly
+            node.children[0].x =
+                node.children[1].x + node.children[1].width + paddingAmount;
+        }
     }
-    else if (node.children[0].x > node.children[1].x &&
-        node.children[1].x + node.children[1].width + paddingAmount !==
-            node.children[0].x) {
-        //The first element is on the right, adjust the x value of the first element accordingly
-        node.children[0].x =
-            node.children[1].x + node.children[1].width + paddingAmount;
+    else if (relation(node.children) == "vertical") {
+        if (node.children[0].y < node.children[1].y &&
+            node.children[0].y + node.children[0].height + paddingAmount !==
+                node.children[1].y) {
+            //The first element is on the left, adjust the x value of the second element accordingly
+            node.children[1].y =
+                node.children[0].y + node.children[0].height + paddingAmount;
+        }
+        else if (node.children[0].y > node.children[1].y &&
+            node.children[1].y + node.children[1].height + paddingAmount !==
+                node.children[0].y) {
+            //The first element is on the right, adjust the x value of the first element accordingly
+            node.children[0].y =
+                node.children[1].y + node.children[1].height + paddingAmount;
+        }
     }
 }
 figma.closePlugin();
+function relation(items) {
+    var margin = 24;
+    //if they're horizontally laid out
+    if (items[0].y + margin >= items[1].y && items[0].y - margin <= items[1].y) {
+        return "horizontal";
+    }
+    else if (items[0].x + margin >= items[1].x && items[0].x - margin <= items[1].x) {
+        return "vertical";
+    }
+}
