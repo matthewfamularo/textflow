eb29653470ab15500ac820179439f125c5bfe4c8
matthewfamularo <matthewfamularo@icloud.com>
2019-08-21 13:00:14 -0400

    makes code more efficient

--- code.js
+++ code.js
@@ -1,17 +1,17 @@
 //Use Figma's traversal helper function to find all textGroup groups
 var nodes = figma.currentPage.findAll(function (node) { return node.name.includes("textGroup") && node.children.length == 2 && node.type === "GROUP"; });
-console.log(nodes);
 for (var _i = 0, nodes_1 = nodes; _i < nodes_1.length; _i++) {
     var node = nodes_1[_i];
     //Set the paddingAmount based on the number specified in the layer name
     var paddingAmount = parseInt(node.name.split("-")[1]);
     //Figure out which element is on the right and which is on the left
-    if (node.children[0].x < node.children[1].x) {
+    //Also ensures that the items aren't already in the right place
+    if (node.children[0].x < node.children[1].x && node.children[0].x + node.children[0].width + paddingAmount !== node.children[1].x) {
         //The first element is on the left, adjust the x value of the second element accordingly
         node.children[1].x =
             node.children[0].x + node.children[0].width + paddingAmount;
     }
-    else {
+    else if (node.children[0].x > node.children[1].x && node.children[1].x + node.children[1].width + paddingAmount !== node.children[0].x) {
         //The first element is on the right, adjust the x value of the first element accordingly
         node.children[0].x =
             node.children[1].x + node.children[1].width + paddingAmount;
--- code.ts
+++ code.ts
@@ -2,16 +2,16 @@
 const nodes = figma.currentPage.findAll(
   node => node.name.includes("textGroup") && node.children.length == 2 && node.type === "GROUP"
 );
-console.log(nodes);
 for (const node of nodes) {
   //Set the paddingAmount based on the number specified in the layer name
   let paddingAmount = parseInt(node.name.split("-")[1]);
   //Figure out which element is on the right and which is on the left
-  if (node.children[0].x < node.children[1].x) {
+  //Also ensures that the items aren't already in the right place
+  if (node.children[0].x < node.children[1].x && node.children[0].x + node.children[0].width + paddingAmount !== node.children[1].x) {
     //The first element is on the left, adjust the x value of the second element accordingly
     node.children[1].x =
       node.children[0].x + node.children[0].width + paddingAmount;
-  } else {
+  } else if(node.children[0].x > node.children[1].x && node.children[1].x + node.children[1].width + paddingAmount !== node.children[0].x) {
     //The first element is on the right, adjust the x value of the first element accordingly
     node.children[0].x =
       node.children[1].x + node.children[1].width + paddingAmount;
